c481c0dcb78a83185c12cb9a9605aac82e93399d
nick thomas <nichoth@gmail.com>
2023-04-28 22:05:47 -0700

    cloudinary CLI works

--- bin/to-string.js
+++ bin/to-string.js
@@ -6,11 +6,11 @@ import { hideBin } from 'yargs/helpers'
 
 const args = yargs(hideBin(process.argv))
     .demandCommand(2)
-    .command('cloudName', 'the cloud to use to get the file')
-    .command('filename', 'the filename to fetch')
-    .example('$0 name my-file.jpg', 'Use the the cloudinary namespace `name`' +
-        'to get and convert `my-file.jpg` to a small base64 string')
-    .usage('Usage: $0 <cloudName> <filename>')
+    .command('cloudName filename', 'the cloud name and filename to use')
+    // .command('filename', 'the filename to fetch')
+    .example('`image name my-file.jpg`', 'Use the the cloudinary namespace `name`' +
+        ' to convert `my-file.jpg` to a small base64 string')
+    .usage('Usage: image <cloudName> <filename>')
     .argv
 
 const cloudName = args._[0]
